9093d3a83dabb8093de3d47e2af6e8a02b8c0f0e
tix04 <04tiananet@gmail.com>
2021-06-07 23:14:51 +0300

    feat: Updated database to remote database

--- middleware/databaseConnection.js
+++ middleware/databaseConnection.js
@@ -1,11 +1,26 @@
 const mySql = require('mysql');
 
+/*
+//Local Database
 const pool = mySql.createPool({ 
     connectionLimit : 50,  //Maximum connection allowed.
     host            : 'localhost', //Where database is hosted
     user            : 'root',      //username
     password        : '',          //password
     database        : 'grouporama', //Which database to access
+    multipleStatements: true
+  });
+
+  module.exports = pool;
+  */
+
+  //Remote Database
+  const pool = mySql.createPool({ 
+    connectionLimit : 50,  //Maximum connection allowed.
+    host            : 'remotemysql.com', //Where database is hosted
+    user            : 'dOjC9ES8Sr',      //username
+    password        : 'R3jYdjbT10',          //password
+    database        : 'dOjC9ES8Sr', //Which database to access
     multipleStatements: true
   });
 

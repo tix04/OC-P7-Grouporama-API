e82d7e2da8e6f10b1ef10c80f6cc4adf6e001b6d
tix04 <04tiananet@gmail.com>
2021-06-08 22:02:54 +0300

    fix: Deleted local database connection

--- middleware/databaseConnection.js
+++ middleware/databaseConnection.js
@@ -1,19 +1,5 @@
 const mySql = require('mysql');
 
-//Local Database
-const pool = mySql.createPool({ 
-    connectionLimit : 50,  //Maximum connection allowed.
-    host            : 'localhost', //Where database is hosted
-    user            : 'root',      //username
-    password        : '',          //password
-    database        : 'grouporama', //Which database to access
-    multipleStatements: true
-  });
-
-  module.exports = pool;
-  
-
-  /*
   //Remote Database
   const pool = mySql.createPool({ 
     connectionLimit : 50,  //Maximum connection allowed.
@@ -25,4 +11,3 @@ const pool = mySql.createPool({
   });
 
   module.exports = pool;
-  */
